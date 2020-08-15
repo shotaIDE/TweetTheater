98f80bfcbb73d3ba5b1c672007bf7f42b9a38b7b
Shota Ide <ide.shota.dev@gmail.com>
2020-08-16 02:14:46 +0900

    デバッグ実行スクリプトが環境変数の変更に追従していない問題を修正

--- create_favorites.py
+++ create_favorites.py
@@ -5,11 +5,11 @@ import os
 from api.twitter import favorite
 
 if __name__ == "__main__":
-    consumer_key = os.environ.get('CONSUMER_KEY')
-    consumer_secret = os.environ.get('CONSUMER_SECRET')
+    consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+    consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
 
-    access_token = os.environ.get('ACCESS_TOKEN')
-    access_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+    access_token = os.environ.get('TWITTER_USER_ACCESS_TOKEN')
+    access_secret = os.environ.get('TWITTER_USER_SECRET')
 
     favorite.create_at_once(
         consumer_key=consumer_key,
