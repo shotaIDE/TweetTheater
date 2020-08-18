c2958f63a174fe74583afb1ddfe5ee2ff6a54626
Shota Ide <ide.shota.dev@gmail.com>
2020-08-18 22:20:56 +0900

    Firebaseを本番環境と切り替えられるように環境変数から取得するように修正

--- src/FirebaseConfig.ts
+++ src/FirebaseConfig.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBnnsai_J9oEmfs4XS8H7FrVzaKmiC_KQM",
-  authDomain: "autoplayclient-dev.firebaseapp.com",
-  databaseURL: "https://autoplayclient-dev.firebaseio.com",
-  projectId: "autoplayclient-dev",
-  storageBucket: "autoplayclient-dev.appspot.com",
-  messagingSenderId: "793287019774",
-  appId: "1:793287019774:web:e9674fb31ca75f3d9a29af",
-  measurementId: "G-SQ81DJLTJ6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
