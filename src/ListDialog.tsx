f39dd34bca45bc6742ac17d414b6b62b861682f6
Shota Ide <ide.shota.dev@gmail.com>
2020-08-24 02:51:30 +0900

    ダイアログの背景色の境目がおかしいので一旦デフォルト色に修正

--- src/ListDialog.tsx
+++ src/ListDialog.tsx
@@ -25,7 +25,6 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       display: "flex",
       justifyContent: "center",
-      backgroundColor: theme.palette.background.default,
     },
   })
 );
